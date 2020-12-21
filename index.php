c0c6f4a21549624cab7455bcc0d0125c0d7ecdca
rahulhassan <specialistrahul@gmail.com>
2020-12-21 09:54:55 +0600

    Fix index page

--- index.php
+++ index.php
@@ -6,102 +6,98 @@
 		<link rel="stylesheet" href="css/main.css">
 	</head>
 	<body>
-      <div class="wr apper">
-        <!--NAVIGATION-->
-        <nav>
-			<div class="navhead">
+      <!--NAVIGATION-->
+      <nav>
+         <div class="navhead">
                <img src="img/logo.png" width ="6%" alt="login" >
                <h4>"Eastern Prime University"</h4>
-			   <h6>-Bringing Ambition of life</h6>
-			</div>
-			<div class="wrapper">
-				<ul>
-					<li>
-						<a class="navitem" href="#">Home</a>
-					</li>
-					<li>
-						<a class="navitem" href="#"> Admission</a>
-					</li>
-					<li>
-						<a class="navitem" href="#">Academics</a>
-					</li>
-					<li>
-						<a class="navitem" href="#">Department</a>
-					</li>
-					<li>
-						<a class="navitem" href="#">Contact Us</a>
-					</li>
-					<li>
-						<a class="navitem" href="#">Login</a>
-						<ul>
-							<li>
-								<a class="navitem" href="admin_login.php">Admin</a>
-							</li>
-							<li>
-								<a class="navitem" href="#">Faculty</a>
-							</li>
-							<li>
-								<a class="navitem" href="#">Student</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-        </nav>
-		<br><br><br><br>
-		
-        <!--END OF NAVIGATION    -->
-        <div class="section">
-            <h1>Bringing Ambition To Life</h1>
-            <p>
-               Eastern Prime University is a private Christian university with its main campus in Searcy, Arkansas. It is the largest private university in Arkansas. Established in 1924, the university offers undergraduate, graduate, and pre-professional programs. The university also comprises  School of Theology, located in Memphis, Tennessee, which was formerly known as Eastern Graduate School of Religion. Eastern  is one of several institutions of higher learning associated with the Churches of Christ.
-            </p>
-            <!--FEATURES-->
-            <div class="features">
-               <h1>Learn Online Class </h1>
-               <img src="img/on.png" width="15%" alt ="on">
-               <h1>NEWS</h1>
-               <img src="img/news.png" width="15%" alt ="news">
-               <h1>Notices</h1>
-               <img src="img/not.png" width="15%" alt ="not"><br>
-               <a href="#">Academic Calender</a><br>
-               <a href="#">Convocation</a><br>
-               <a href="#">General instructions of online classes</a>
-               <h1>Admission</h1>
-               <img src="img/add.png" width="15%" alt ="add">
-            </div>
-            <!--END OF FEATURES-->
-        </div>
-        <!-- END OF SECTION-->
-        </div>
-        <!--END OF WRAPPER-->
-        <div>
-            <h1>POPULAR COURSES WE OFFER </h1>
-            <div class="courses">
-				<div class="CSE">
-				   <h3>Computer  Science and Engineering</h3>
-				   <img src="img/cs.png" width ="15%" alt="cse">
-				   <a class="btn1"href="#"><button class="apply">Apply Here </a>
-				</div>
-            <div class="busniess">
-               <h3>Business Studies</h3>
-               <img src= "img/bus.jpg" width ="15%" alt="business">
-               <a class="btn1" href="#"><button class="apply">Apply Here </a>
-            </div>
-            <div class="journalism">
-               <h3>Journalism Studies</h3>
-               <img src="img/jou.png" width ="15%" alt="journalism">
-               <a class="btn1" href="#"><button class="apply">Apply Here </a>
-            </div>
-            <div class="Electric">
-               <h3>Electric and Electronic Engineering</h3>
-               <img src="img/eee.png" width ="15%" alt="eee">
-               <a class="btn1" href="#"><button class="apply">Apply Here</a>
-            </div>
-            <h3>Quick Links</h3>
-            <h3>Address and Details</h3>
-            <h3>Contact</h3>
+            <h6>-Bringing Ambition of life</h6>
          </div>
+         <div class="wrapper">
+            <ul>
+               <li>
+                  <a class="navitem" href="#">Home</a>
+               </li>
+               <li>
+                  <a class="navitem" href="#"> Admission</a>
+               </li>
+               <li>
+                  <a class="navitem" href="#">Academics</a>
+               </li>
+               <li>
+                  <a class="navitem" href="#">Department</a>
+               </li>
+               <li>
+                  <a class="navitem" href="#">Contact Us</a>
+               </li>
+               <li>
+                  <a class="navitem" href="#">Login</a>
+                  <ul>
+                     <li>
+                        <a class="navitem" href="admin_login.php">Admin</a>
+                     </li>
+                     <li>
+                        <a class="navitem" href="#">Faculty</a>
+                     </li>
+                     <li>
+                        <a class="navitem" href="#">Student</a>
+                     </li>
+                  </ul>
+               </li>
+            </ul>
+         </div>
+      </nav>
+      <br><br><br><br>
+   
+      <!--END OF NAVIGATION    -->
+      <div class="section">
+         <h1>Bringing Ambition To Life</h1>
+         <p>
+            Eastern Prime University is a private Christian university with its main campus in Searcy, Arkansas. It is the largest private university in Arkansas. Established in 1924, the university offers undergraduate, graduate, and pre-professional programs. The university also comprises  School of Theology, located in Memphis, Tennessee, which was formerly known as Eastern Graduate School of Religion. Eastern  is one of several institutions of higher learning associated with the Churches of Christ.
+         </p>
+         <!--FEATURES-->
+         <div class="features">
+            <h1>Learn Online Class </h1>
+            <img src="img/on.png" width="15%" alt ="on">
+            <h1>NEWS</h1>
+            <img src="img/news.png" width="15%" alt ="news">
+            <h1>Notices</h1>
+            <img src="img/not.png" width="15%" alt ="not"><br>
+            <a href="#">Academic Calender</a><br>
+            <a href="#">Convocation</a><br>
+            <a href="#">General instructions of online classes</a>
+            <h1>Admission</h1>
+            <img src="img/add.png" width="15%" alt ="add">
+         </div>
+         <!--END OF FEATURES-->
+      </div>
+
+      <div>
+         <h1>POPULAR COURSES WE OFFER </h1>
+         <div class="courses">
+         <div class="CSE">
+            <h3>Computer  Science and Engineering</h3>
+            <img src="img/cs.png" width ="15%" alt="cse">
+            <a class="btn1"href="#"><button class="apply">Apply Here </a>
+         </div>
+         <div class="busniess">
+            <h3>Business Studies</h3>
+            <img src= "img/bus.jpg" width ="15%" alt="business">
+            <a class="btn1" href="#"><button class="apply">Apply Here </a>
+         </div>
+         <div class="journalism">
+            <h3>Journalism Studies</h3>
+            <img src="img/jou.png" width ="15%" alt="journalism">
+            <a class="btn1" href="#"><button class="apply">Apply Here </a>
+         </div>
+         <div class="Electric">
+            <h3>Electric and Electronic Engineering</h3>
+            <img src="img/eee.png" width ="15%" alt="eee">
+            <a class="btn1" href="#"><button class="apply">Apply Here</a>
+         </div>
+         <h3>Quick Links</h3>
+         <h3>Address and Details</h3>
+         <h3>Contact</h3>
       </div>
 	</body>
 </html>
